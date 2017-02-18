53d32178d94a68e0330ae43737e336feb73a43de
codesboy <565195693@qq.com>
2017-02-18 15:55:42 +0800

    add files download function

--- application/user/controller/Index.php
+++ application/user/controller/Index.php
@@ -20,4 +20,35 @@ class Index extends Controller
         ->select();
         return json($userdata);
     }
+
+    // 文件下载方法
+    /*public function downloads($file=null){
+        // 未传入参数
+        if(!$file){
+            return $this->fetch('download');
+            exit;
+        }
+        // dump($file);
+        $file_dir = ROOT_PATH.'public/dl/'.$file;
+        // dump($file_dir);
+        $filename=pathinfo($file);
+        // dump($filename);
+
+        if (!file_exists($file_dir)){
+            header("Content-type: text/html; charset=utf-8");
+            echo "File not found!";
+            exit;
+        } else {
+            $file = fopen($file_dir,"r");
+            header("Content-type: application/octet-stream");
+            header("Accept-Ranges: bytes");
+            header("Accept-Length: ".filesize($file_dir));
+            header("Content-Disposition: attachment; filename=".$filename['basename']);
+            echo fread($file, filesize($file_dir));
+            fclose($file);
+        }
+    }*/
+
+
+
 }
